7cf30e91f266d8b320ee4ce90ffe1122b4f93c01
TreyAnizi <tracyaniziamoro1@gmail.com>
2025-02-20 16:01:08 +0000

    second commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,46 @@
+const button = document.getElementById('btn')
+button.addEventListener('click',() => {
+   // alert('Hey')
+   
+   fetchData('https://jsonplaceholder.typicode.com/users')
+})
 
 
-fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(json => console.log(json))
+
+
+
+
+
+
+
+
+
+async function fetchData(url) {
+      // fetch the data
+   const data = await fetch(url)
+//    await the promise to get your response
+   const response = await data.json()
+   console.log(response)  
+   
+   console.log(response[0])
+
+   response.map(
+      (user) => {console.log(user)
+
+            console.log(user.username)
+            console.log(user.company.name)
+            console.log(user.address.geo.lat)
+            console.log(user.company.catchPhrase)
+            console.log(user.address.city)
+
+   }
+)
+}
+
+
+
+// fetch('https://jsonplaceholder.typicode.com/users')
+//       .then(response => response.json())
+//       .then(json => console.log(json))
 
       
